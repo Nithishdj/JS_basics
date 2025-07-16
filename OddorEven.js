671b13630ba9b08815d045d447270704247ca945
Nithish D <146643466+Nithishdj@users.noreply.github.com>
2025-07-16 16:38:01 +0530

    Add files via upload

---
+++ OddorEven.js
@@ -0,0 +1,10 @@
+let x=30;
+if(x%2==0){
+    console.log("The number is even");
+}
+else if(x%5==0){
+    console.log("The number is divisible by 5");
+}
+else{
+    console.log("The number is odd");
+}
