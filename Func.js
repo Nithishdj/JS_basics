83e6a45f501e9b0b230a9bfa8bd308febf2224ff
Nithish D <146643466+Nithishdj@users.noreply.github.com>
2025-07-18 18:51:29 +0530

    Add files via upload

---
+++ Func.js
@@ -0,0 +1,23 @@
+const sla=() =>//using big arrow
+{
+    console.log("Welcome to sla");
+}
+sla();
+
+//arrow func with single parameter
+const s=x=>x*x;
+console.log(s(5));
+
+const add=(a,b)=>a+b;
+console.log(add(2,3));
+
+//Arrow function with multiple parameter
+const s1=(x1,y1,z1) =>
+{
+    console.log(x1+y1+z1);
+}
+s1(5,42);
+
+
+
+
