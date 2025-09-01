e0a3382a8ce63efbf5ddf3f70fe44445cc2c8f01
Nithish D <146643466+Nithishdj@users.noreply.github.com>
2025-09-01 19:40:46 +0530

    Add files via upload

---
+++ TextToSpeech.js
@@ -0,0 +1,5 @@
+ function speak(){
+            const input = document.getElementById('text').value;
+            const utterance = new SpeechSynthesisUtterance(input);
+            speechSynthesis.speak(utterance);
+        }
